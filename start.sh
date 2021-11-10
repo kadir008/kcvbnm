63222cf0127f664fe721da73854d95f2d130a85a
kadir008 <86116178+kadir008@users.noreply.github.com>
2021-11-10 19:57:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/kadir008/kfjkgfdjgd -b $BRANCH /kfjkgfdjgd
 fi
-cd /VCPlayerBot
+cd /kfjkgfdjgd
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
